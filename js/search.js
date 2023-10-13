c6248f44c74b28f619194aca599b011312399050
Tieg O'Sullivan <tiegosullivanpsnl@gmail.com>
2023-10-13 19:03:16 +0100

    tmp

---
+++ js/search.js
@@ -0,0 +1,15 @@
+/*
+FIXME: Big list of FIXMEs
+- TS instead of JS
+
+*/
+
+function search() {
+    return dummyData; // TODO: add logic to call endpoints and get proper data  
+}
+
+// FIXME: this will actually become part of an event listener
+const searchResults = search();
+
+
+// FIXME: this will move to 
